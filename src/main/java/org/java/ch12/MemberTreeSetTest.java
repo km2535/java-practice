c136c4ba8d3792beca26fce4e783f3a96836d05d
Lee kangmin <km2535@naver.com>
2024-10-30 20:08:33 +0900

    practice: 챕터 12

--- src/main/java/org/java/ch12/MemberTreeSetTest.java
+++ src/main/java/org/java/ch12/MemberTreeSetTest.java
@@ -1,9 +1,10 @@
 package org.java.ch12;
 
 public class MemberTreeSetTest {
+
 	public static void main(String[] args) {
 		MemberTreeSet memberTreeSet = new MemberTreeSet();
-
+		String str = "Dd";
 		Member memberLee = new Member(100,"lee");
 		Member memberMale = new Member(200,"male");
 		Member memberFemale = new Member(300,"female");
